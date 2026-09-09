96e43a85b1038d40a779704b2df4eb1217326724
MeZiul <luizguilhermy83302@gmail.com>
2026-09-09 12:33:40 -0400

    admin/layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,4 +3,12 @@
 // Preciso testar os componentes de adimin, essa página será refeita posteriormente!!
 // Será copiado do layout de app/layout.tsx
 
+export default function AdminLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return children;
+}
+
 
